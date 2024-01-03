f76ac793771407d89640595a77778cf56ef49a72
matt <leithsceal@hotmail.com>
2024-01-03 15:09:11 +0100

    représentation

--- vmetre.js
+++ vmetre.js
@@ -5,12 +5,12 @@ if('serviceWorker' in navigator)
 function melanger()
 {
   var tableau = ['<input type="checkbox" id="1" name="A1" value="1">Remarques et critiques acceptées<br/>', '<input type="checkbox" id="2" name="A2" value="1">Promotions pour les femmes comme pour les hommes<br/>', '<input type="checkbox" id="3" name="A3" value="1">Travail en confiance et autonomie<br/>', '<input type="checkbox" id="4" name="A4" value="1">Reconnaissance du travail<br/>', '<input type="checkbox" id="5" name="A5" value="1">Refus de relations extraprofessionnelles accepté<br/>', '<input type="checkbox" id="6" name="B1" value="1">Commentaires sur l\'apparence<br/>', '<input type="checkbox" id="7" name="B2" value="1">Parole coupée systématiquement<br/>', '<input type="checkbox" id="8" name="B3" value="1">Blague sur les promotions canapé<br/>', '<input type="checkbox" id="9" name="B4" value="1">Questions indiscrètes insistantes sur la vie privée<br/>', '<input type="checkbox" id="10" name="B5" value="1">Blagues sexistes sur les blondes<br/>', '<input type="checkbox" id="11" name="B6" value="1">Evocation de sexualité sans accord<br/>', '<input type="checkbox" id="12" name="B7" value="1">Mécontentement après refus d\'être raccompagnée<br/>', '<input type="checkbox" id="13" name="B8" value="1">Recherche systématique d\'être seul avec une femme<br/>', '<input type="checkbox" id="14" name="C1" value="1">Images à caractère pornographique visibles<br/>', '<input type="checkbox" id="15" name="C2" value="1">Regards insistants sur la poitrine et les fesses<br/>', '<input type="checkbox" id="16" name="C3" value="1">SMS ou courriels à caractère sexuel sans accord<br/>', '<input type="checkbox" id="17" name="C4" value="1">Demande insistante d\'un acte sexuel<br/>', '<input type="checkbox" id="18" name="C5" value="1">Hostilité liée au refus d\'un acte sexuel<br/>', '<input type="checkbox" id="19" name="D1" value="1">Menaces professionnelles pour obtenir un acte sexuel<br/>', '<input type="checkbox" id="20" name="D2" value="1">Baiser forcé ou par surprise<br/>', '<input type="checkbox" id="21" name="D3" value="1">Toucher les seins, fesses ou cuisses sans consentement<br/>', '<input type="checkbox" id="22" name="E1" value="1">Fellation ou pénétration forcée<br/>'];
-  for (i = tableau.length - 1; i > 0; i--)
+  for (var i = tableau.length - 1; i > 0; i--)
   {
     var j = Math.floor(Math.random() * (i + 1));
     [tableau[i], tableau[j]] = [tableau[j], tableau[i]];
   }
-  for (n=0;n<tableau.length;n++)
+  for (var n=0;n<tableau.length;n++)
   {
     document.getElementById("formulaire").innerHTML += tableau[n];
   }
@@ -23,7 +23,7 @@ function calcul()
   var C = 0;
   var D = 0;
   var E = 0;
-  for (i=1;i<23;i++)
+  for (var i=1;i<23;i++)
   {
     if (document.getElementById("" + i).checked)
     {
@@ -48,76 +48,123 @@ function calcul()
       }
     }
   }
-  //var vtheta = ["Viols", "Agressions sexuelles", "Harcèlement sexuel", "Environnement pro sexiste et hostile", "Environnement pro sain"];
-  var vtheta = ["V", "A", "H", "SH", "OK"];
-  var data = [{
-    r: [E*100, 0, 0, 0, 0],
-    theta: vtheta,
-    name: "Viols",
-    marker: {color: "rgb(0,0,0)"},
-    type: "barpolar",
-    hoverinfo: "name"
-  },
+  if (A+B+C+D+E != 0)
   {
-    r: [0, D/3*100, 0, 0, 0],
-    theta: vtheta,
-    name: "Agressions sexuelles",
-    marker: {color: "rgb(255, 0, 36)"},
-    type: "barpolar",
-    hoverinfo: "name"
-  },
-           {
-    r: [0, 0, C/5*100, 0, 0],
-    theta: vtheta,
-    name: "Harcèlement sexuel",
-    marker: {color: "rgb(255, 147, 51)"},
-    type: "barpolar",
-    hoverinfo: "name"
-  },
-  {
-    r: [0, 0, 0, B/8*100, 0],
-    theta: vtheta,
-    name: "Environnement pro sexiste et hostile",
-    marker: {color: "rgb(252, 208, 0)"},
-    type: "barpolar",
-    hoverinfo: "name"
-  },   
-  {
-    r: [0, 0, 0, 0, A/5*100],
-    theta: vtheta,
-    name: "Environnement pro sain",
-    marker: {color: "rgb(36, 140, 70)"},
-    type: "barpolar",
-    hoverinfo: "name"
-  }]
-var layout = {
-    paper_bgcolor: '#1e1e1e',
-    font: {size: 12, color: '#cdcccc'},
-    margin: {
-      b: 10,
-      l: 10,
-      r: 10,
-      t: 50
+    var rang;
+    var rE = [0,0,0,0,0];
+    var rD = [0,0,0,0,0];
+    var rC = [0,0,0,0,0];
+    var rB = [0,0,0,0,0];
+    var rA = [0,0,0,0,0];
+
+    if (A != 0) {A = A*4; rang = 0;};
+    if (B != 0) {B = B*5/2; rang = 1;};
+    if (C != 0) {C = C*4; rang = 2};
+    if (D != 0) {D = 20; rang = 3};
+    if (E != 0) {E = 20; rang = 4};
+
+    rE[rang] = E;
+    rD[rang] = D;
+    rC[rang] = C;
+    rB[rang] = B;
+    rA[rang] = A;
+    
+    //var vtheta = ["Viols", "Agressions sexuelles", "Harcèlement sexuel", "Environnement pro sexiste et hostile", "Environnement pro sain"];
+    var vtheta = ["OK", "SH", "H", "A", "V"];
+    var data = [{
+      r: rA,
+      theta: vtheta,
+      name: "Environnement pro sain",
+      //marker: {color: couleur(A, "OK")},
+      marker: {color: "green"},
+      type: "barpolar",
+      hoverinfo: "name"
     },
-    legend: {
-      x: 0.2,
-      y: -0.5
+    {
+      r: rB,
+      theta: vtheta,
+      name: "Environnement pro sexiste et hostile",
+      //marker: {color: couleur(B, "SH")},
+      marker: {color: "yellow"},
+      type: "barpolar",
+      hoverinfo: "name"
     },
-    showlegend: true,
-    polar: {
-      bgcolor: "#d7d9dc",
-      barmode: "overlay",
-      bargap: 0,
-      radialaxis: {ticks: "", showline: false, showticklabels: false, range: [0,100]},
-      angularaxis: {direction: "clockwise"}
+    {
+      r: rC,
+      theta: vtheta,
+      name: "Harcèlement sexuel",
+      //marker: {color: couleur(C, "H")},
+      marker: {color: "orange"},
+      type: "barpolar",
+      hoverinfo: "name"
     },
-    autosize: true
+    {
+      r: rD,
+      theta: vtheta,
+      name: "Agression sexuelle",
+      //marker: {color: couleur(D, "A")},
+      marker: {color: "red"},
+      type: "barpolar",
+      hoverinfo: "name"
+    },
+    {
+      r: rE,
+      theta: vtheta,
+      name: "Viol",
+      //marker: {color: couleur(E, "V")},
+      marker: {color: "black"},
+      type: "barpolar",
+      hoverinfo: "name"
+    }
+    ];
+    var layout = {
+        paper_bgcolor: '#1e1e1e',
+        font: {size: 12, color: '#cdcccc'},
+        margin: {
+          b: 10,
+          l: 10,
+          r: 10,
+          t: 50
+        },
+        legend: {
+          x: 0.2,
+          y: -0.5
+        },
+        showlegend: true,
+        polar: {
+          bgcolor: "#d7d9dc",
+          barmode: "stack",
+          bargap: 0,
+          radialaxis: {ticks: "", showline: false, showticklabels: false},
+          angularaxis: {direction: "clockwise"}
+        },
+        autosize: true
+      };
+    var config = {
+        staticPlot: true,
+        responsive: true
+    };
+    Plotly.newPlot("rose", data, layout, config);
   }
-var config = {
-    staticPlot: true,
-    responsive: true
 }
-Plotly.newPlot("rose", data, layout, config)
+function couleur(score, categorie)
+{
+  if (score == 0)
+  {
+    return "#d7d9dc";
+  }
+  else
+  {
+    const hachagedefaut = "#d7d9dc";
+    const hachagecategorie = {
+      "V": "black",
+      "A": "red",
+      "H": "orange",
+      "SH": "yellow",
+      "OK": "green"
+    };
+    return hachagecategorie[categorie] || hachagedefaut;
+  }
 }
 function opentab(evt, tabname)
 {
